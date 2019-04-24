319eeaee74d9993f10db9bed6c1cc0865f7d3ec5
T L Goddard <TGoddard@spartaglobal.com>
2019-04-24 16:14:37 +0100

    Initial setup complete

---
+++ src/main/java/com/spartaglobal/tlg/JSONJackson/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.tlg.JSONJackson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
